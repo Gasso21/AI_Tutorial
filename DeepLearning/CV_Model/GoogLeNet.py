df95583024ce4474e4981074b8d018d7b2a7f447
Gasso21 <jhjung1227@naver.com>
2023-03-18 12:50:23 +0900

    GoogLeNet

--- DeepLearning/CV_Model/GoogLeNet.py
+++ DeepLearning/CV_Model/GoogLeNet.py
@@ -1,1 +1,8 @@
-a
+from tensorflow import keras
+from keras.layers import Conv2D, MaxPool2D, concatenate,
+def inception_module(x, filters_1x1,
+                     filters_3x3_reduce, filters_3x3,
+                     filters5x5_reduce, filters_5x5,
+                     filters_pool_proj, name=None):
+
+    conv_1x1 = Conv2D(filters=filters_1
